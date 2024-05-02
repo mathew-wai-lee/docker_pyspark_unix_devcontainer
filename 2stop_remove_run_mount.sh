b4225f5659f97f1f68551fc814db5e3974386c39
mwlee9 <mwlee9@gmail.com>
2024-05-02 10:48:21 -0500

    Add a demo file weather data

--- 2stop_remove_run_mount.sh
+++ 2stop_remove_run_mount.sh
@@ -2,5 +2,10 @@ finch stop pyspark
 
 finch rm pyspark
 
-finch run -p 8888:8888 -p 4040:4040 --name pyspark lee/pyspark jupyter lab --ip=0.0.0.0 --port=8888 --NotebookApp.token='' --allow-root
+finch run \
+    -p 8888:8888 -p 4040:4040 \
+    --name pyspark \
+    -v "$(pwd)/VOLUMES:/home/VOLUMES" \
+    lee/pyspark \
+    jupyter lab --ip=0.0.0.0 --port=8888 --NotebookApp.token='' --allow-root
 
